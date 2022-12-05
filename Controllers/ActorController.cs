581dca93490f9156b867fc940883dba92724cab3
GiulsV <valente.giulia93@gmail.com>
2022-12-05 16:29:09 +0100

    Creazione controller

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_boolflix.Controllers
+{
+    public class ActorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
